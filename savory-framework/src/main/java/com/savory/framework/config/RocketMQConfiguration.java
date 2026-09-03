e61a5254171b02f88a1662d9c685b710a5057d09
米饭是菜 <16782265+mifan-shicai@user.noreply.gitee.com>
2026-09-03 13:00:49 +0800

    feat(mq): add TransactionMQProducer bean

--- savory-framework/src/main/java/com/savory/framework/config/RocketMQConfiguration.java
+++ savory-framework/src/main/java/com/savory/framework/config/RocketMQConfiguration.java
@@ -3,6 +3,7 @@ package com.savory.framework.config;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.rocketmq.client.exception.MQClientException;
 import org.apache.rocketmq.client.producer.DefaultMQProducer;
+import org.apache.rocketmq.client.producer.TransactionMQProducer;
 import org.apache.rocketmq.client.consumer.DefaultMQPushConsumer;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
@@ -29,6 +30,20 @@ public class RocketMQConfiguration {
         producer.setSendMsgTimeout(5000);
         producer.setRetryTimesWhenSendFailed(2);
         log.info("RocketMQ Producer 初始化成功, NameServer: {}", nameServer);
+        return producer;
+    }
+
+    /**
+     * 事务消息 Producer：与普通 producer 用不同 group，避免消息路由冲突。
+     * TransactionListener 由业务方在发送时通过 sendMessageInTransaction 注入。
+     */
+    @Bean(initMethod = "start", destroyMethod = "shutdown")
+    public TransactionMQProducer rocketMQTransactionProducer() throws MQClientException {
+        TransactionMQProducer producer = new TransactionMQProducer(
+                producerGroup + "-tx");
+        producer.setNamesrvAddr(nameServer);
+        producer.setSendMsgTimeout(5000);
+        log.info("RocketMQ TransactionProducer 初始化成功, NameServer: {}", nameServer);
         return producer;
     }
 }
